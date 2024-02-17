fee65b537b016184ae6d9f9bde9b941b6303103f
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-02-17 21:20:34 +0530

    ADD: Business Website

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-// import Features from './components/Features';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import Home1 from './components/Home';
 import Projects from './components/Projects';
 import Service from './components/Service';
-// import Team from './components/Team';
-// import Testimonials from './components/Testimonials';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
@@ -25,17 +20,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       </BrowserRouter>
-      {/* <Header /> */}
-      {/* <Home1 /> */}
-      {/* <About /> */}
-      {/* <Service /> */}
-      {/* <Features /> */}
-      {/* <Projects /> */}
-      {/* <Team /> */}
-      {/* <Testimonials /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
-      {/* <Home /> */}
+      <SpeedInsights url="https://westcoders.vercel.app/" />
     </div>
   );
 }
